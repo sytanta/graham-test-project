aee34f5e530a2d4856b5dc8a4b20128771926ae5
TA SY TAN <sytanta@yahoo.com>
2025-07-21 19:45:14 +0700

    Add config for test database

--- config/index.ts
+++ config/index.ts
@@ -6,7 +6,7 @@ export default {
   database: {
     host: process.env.DB_HOST || "localhost",
     port: parseInt(process.env.DB_PORT || "3306", 10),
-    name: process.env.DB_NAME || "task-management",
+    name: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
   },
--- tests/setup.ts
+++ tests/setup.ts
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.test" });
+
 import { sequelize } from "../config/database";
 
 beforeAll(async () => {
