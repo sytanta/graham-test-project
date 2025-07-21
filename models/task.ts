6c7bb397f308f9679157823ff97299a6b4ab0aa3
TA SY TAN <sytanta@yahoo.com>
2025-07-21 14:37:22 +0700

    Update task model

--- models/task.ts
+++ models/task.ts
@@ -4,11 +4,12 @@ import { sequelize } from "../config/database";
 import { Task as TaskInterface } from "../types";
 
 export class Task extends Model<TaskInterface> implements TaskInterface {
+  public id?: number;
   public title!: string;
-  public description?: string;
-  public completed!: boolean;
-  public created_at!: Date;
-  public updated_at!: Date;
+  public description!: string;
+  public completed?: boolean;
+  public created_at?: Date;
+  public updated_at?: Date;
 }
 
 Task.init(
@@ -16,14 +17,31 @@ Task.init(
     title: {
       type: DataTypes.STRING(255),
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Title is required",
+        },
+      },
     },
     description: {
       type: DataTypes.TEXT,
-      allowNull: true,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Description is required",
+        },
+      },
     },
     completed: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
+      validate: {
+        customValidator(value: any) {
+          if (typeof value !== "boolean") {
+            throw new Error("Completed must be a boolean value");
+          }
+        },
+      },
     },
     created_at: {
       type: DataTypes.DATE,
@@ -33,6 +51,7 @@ Task.init(
     updated_at: {
       type: DataTypes.DATE,
       allowNull: false,
+      defaultValue: Sequelize.fn("now"),
     },
   },
   {
